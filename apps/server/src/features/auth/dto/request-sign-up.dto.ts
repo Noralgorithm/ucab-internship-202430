a91e310ea5135428b34cf9c1d8c5b07e78016ae7
DaikoneKisu <eduardoarzolay@gmail.com>
2024-08-28 23:40:24 -0400

    feat!: :sparkles: validate email in RequestSignUpDto and SignInDto
    BREAKING CHANGES: validations lead to previous correct request to yield bad request error

--- apps/server/src/features/auth/dto/request-sign-up.dto.ts
+++ apps/server/src/features/auth/dto/request-sign-up.dto.ts
@@ -1,9 +1,11 @@
 import { IsEmail, IsString, MaxLength } from 'class-validator'
+import { User } from '~/features/users/entities/user.entity'
 import { EMAIL_MAX_LENGTH } from '~/shared/constants'
+import { NotExists } from '~/shared/validators/not-exists.validator'
 
 export class RequestSignUpDto {
-	//TODO: exists validator
 	//TODO: is ucab or whitelisted email validator
+	@NotExists({ entity: User, message: 'El usuario ya existe' })
 	@MaxLength(EMAIL_MAX_LENGTH)
 	@IsEmail()
 	@IsString()
