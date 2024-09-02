f61634d286171b682fe37e763a3ddff68ae43411
Chocopaula <mariapforero2003@gmail.com>
2024-09-02 19:37:26 -0400

    chore: delete route playground

--- apps/client/src/app/core/app.routes.ts
+++ apps/client/src/app/core/app.routes.ts
@@ -2,14 +2,8 @@ import type { Routes } from '@angular/router'
 import { AuthComponent } from '../pages/auth/auth.component'
 import { LoginComponent } from '../pages/auth/login/login.component'
 import { RegisterComponent } from '../pages/auth/register/register.component'
-import { ComponentPlaygroundComponent } from '../pages/component-playground/component-playground.component'
 
 export const routes: Routes = [
-	{
-		path: 'playground',
-		title: 'Playground',
-		component: ComponentPlaygroundComponent
-	},
 	{
 		path: '',
 		title: 'MoviC',
