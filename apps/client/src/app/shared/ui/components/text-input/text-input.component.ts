adc31a7c0d275ca5f5aa46aafcd987ca982c2c1c
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-09-03 18:33:17 -0400

    feat: text input component functionality

--- apps/client/src/app/shared/ui/components/text-input/text-input.component.ts
+++ apps/client/src/app/shared/ui/components/text-input/text-input.component.ts
@@ -1,12 +1,17 @@
 import { Component, Input } from '@angular/core'
+import { FormControl, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
 	selector: 'text-input',
 	standalone: true,
-	imports: [],
+	imports: [ReactiveFormsModule],
 	templateUrl: './text-input.component.html',
 	styleUrl: './text-input.component.css'
 })
 export class InputTextComponent {
+	@Input({ required: true }) inputId!: string
+	@Input() control: FormControl = new FormControl()
 	@Input() disabled = false
+	@Input() placeholder = ''
+	@Input() label = ''
 }
