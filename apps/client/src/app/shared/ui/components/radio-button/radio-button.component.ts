76004686a90fc1e3e3d8858047b9d6842b0b0e40
Chocopaula <mariapforero2003@gmail.com>
2024-09-04 23:52:47 -0400

    feat: add inputs decorators in radio button component

--- apps/client/src/app/shared/ui/components/radio-button/radio-button.component.ts
+++ apps/client/src/app/shared/ui/components/radio-button/radio-button.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core'
-
+import { Input } from '@angular/core'
+import { FormControl, ReactiveFormsModule } from '@angular/forms'
 @Component({
 	selector: 'radio-button',
 	standalone: true,
-	imports: [],
+	imports: [ReactiveFormsModule],
 	templateUrl: './radio-button.component.html',
 	styleUrl: './radio-button.component.css'
 })
-export class RadioButtonComponent {}
+export class RadioButtonComponent {
+	@Input({ required: true }) inputId!: string
+	@Input() control: FormControl = new FormControl()
+	@Input() value = ''
+	@Input() label = ''
+}
