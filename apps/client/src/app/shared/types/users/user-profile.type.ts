961f567a8b2047023e84bd641d46ce713323cdaa
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-09-08 13:29:11 -0400

    feat: user profile type

---
+++ apps/client/src/app/shared/types/users/user-profile.type.ts
@@ -0,0 +1,19 @@
+import { Gender } from './user-gender.type'
+import { UserRole } from './user-role.type'
+import { UserType } from './user-type.type'
+
+export interface UserProfile {
+	firstName: string
+	lastName: string
+	email: string
+	gender: Gender
+	type: UserType
+	walkDistance: number
+	preferredRole: UserRole
+	profilePicSrc: string
+	phoneNumber: string | null
+	emergencyContactPhoneNumber: string | null
+	isDriver: boolean
+	isActive: boolean
+	isBlocked: boolean
+}
---
+++ apps/client/src/app/shared/types/users/user-role.type.ts
@@ -0,0 +1,1 @@
+export type UserRole = 'driver' | 'passenger'
