b83045b29614ea4e8babb6e1b1437f2ab3ebcff7
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-09-08 14:26:48 -0400

    feat: show user profile data

--- apps/client/src/app/pages/profile/profile.component.ts
+++ apps/client/src/app/pages/profile/profile.component.ts
@@ -1,19 +1,23 @@
+import { JsonPipe } from '@angular/common'
 import { Component } from '@angular/core'
 import { GetOwnProfileService } from '~/features/profile/services/get-own-profile.service'
+import { UserProfile } from '~/shared/types/users/user-profile.type'
 
 @Component({
 	selector: 'app-profile',
 	standalone: true,
-	imports: [],
+	imports: [JsonPipe],
 	templateUrl: './profile.component.html',
 	styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
 	constructor(private readonly getOwnProfileService: GetOwnProfileService) {}
 
+	profile: UserProfile | undefined
+
 	ngOnInit() {
-		this.getOwnProfileService.execute().subscribe((profile) => {
-			console.log(profile)
+		this.getOwnProfileService.execute().subscribe((res) => {
+			this.profile = res.data
 		})
 	}
 }
