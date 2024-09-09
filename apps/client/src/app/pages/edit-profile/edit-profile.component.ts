719f1262c0f5b359b8125881d096fba8c99db0a0
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-09-08 20:35:08 -0400

    feat: edit profile page

--- apps/client/src/app/pages/edit-profile/edit-profile.component.ts
+++ apps/client/src/app/pages/edit-profile/edit-profile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core'
+import { PageLayoutComponent } from '../../shared/ui/components/page-layout/page-layout.component'
 
 @Component({
 	selector: 'app-edit-profile',
 	standalone: true,
-	imports: [],
+	imports: [PageLayoutComponent],
 	templateUrl: './edit-profile.component.html',
 	styleUrl: './edit-profile.component.css'
 })
