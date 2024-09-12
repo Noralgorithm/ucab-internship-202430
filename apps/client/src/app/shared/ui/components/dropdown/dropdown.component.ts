14df89d64f2c6bc0a384e5ed84c194683b9b53e2
Chocopaula <mariapforero2003@gmail.com>
2024-09-12 14:35:44 -0400

    fix: dropdown component

--- apps/client/src/app/shared/ui/components/dropdown/dropdown.component.ts
+++ apps/client/src/app/shared/ui/components/dropdown/dropdown.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
+import { FormControl, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
 	selector: 'dropdown',
 	standalone: true,
-	imports: [],
+	imports: [ReactiveFormsModule],
 	templateUrl: './dropdown.component.html',
 	styleUrl: './dropdown.component.css'
 })
-export class DropdownComponent {}
+export class DropdownComponent {
+	@Input() options: Option[] = []
+	@Input({ required: true }) inputId!: string
+	@Input() label = ''
+	@Input() control: FormControl = new FormControl()
+}
+
+interface Option {
+	value: string
+	label: string
+}
