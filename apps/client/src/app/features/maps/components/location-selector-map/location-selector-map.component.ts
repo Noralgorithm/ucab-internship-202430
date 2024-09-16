be92ff5af7865f19b19f678023d5cd620f84ec80
Chocopaula <mariapforero2003@gmail.com>
2024-09-16 17:43:18 -0400

    feat: add mapMarker in location selector map

--- apps/client/src/app/features/maps/components/location-selector-map/location-selector-map.component.ts
+++ apps/client/src/app/features/maps/components/location-selector-map/location-selector-map.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core'
+import { GoogleMap, MapMarker } from '@angular/google-maps'
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser'
 import { UcabLocationService } from '~/features/maps/ucab-location.service'
+import { environment } from '~env/environment.development'
 
 @Component({
 	selector: 'location-selector-map',
 	standalone: true,
-	imports: [],
+	imports: [GoogleMap, MapMarker],
 	templateUrl: './location-selector-map.component.html',
 	styleUrl: './location-selector-map.component.css'
 })
@@ -17,8 +19,26 @@ export class LocationSelectorMapComponent {
 		private readonly sanitizer: DomSanitizer
 	) {
 		this.ucabLocationService.getUcabLocationString
-		const unsafeMapSrc = `//www.google.com/maps/embed/v1/place?language=es&key=AIzaSyC9VB23WxTYtPIzLMBVMlM0wKWHq12Wh6k&q=${this.ucabLocationService.getUcabLocationString()}`
+		const GOOGLE_MAPS_API_KEY = environment.GOOGLE_MAPS_API_KEY
+		const unsafeMapSrc = `//www.google.com/maps/embed/v1/place?language=es&key=${GOOGLE_MAPS_API_KEY}&q=${this.ucabLocationService.getUcabLocationString()}`
 
 		this.mapSrc = this.sanitizer.bypassSecurityTrustResourceUrl(unsafeMapSrc)
+	}
+
+	center: google.maps.LatLngLiteral = {
+		lat: 8.2973336,
+		lng: -62.7114084
+	}
+	zoom = 4
+	markerOptions: google.maps.MarkerOptions = { draggable: false }
+	markerPosition: google.maps.LatLngLiteral = {
+		lat: 0,
+		lng: 0
+	}
+
+	addMarker(event: google.maps.MapMouseEvent) {
+		if (event.latLng) {
+			this.markerPosition = event.latLng.toJSON()
+		}
 	}
 }
--- apps/client/src/environments/environment.development.ts
+++ apps/client/src/environments/environment.development.ts
@@ -5,5 +5,5 @@ dotenv.config()
 export const environment = {
 	IS_PRODUCTION: true,
 	BACKEND_BASE_URL: 'https://backend.movic.sustentabilidadtech.lat',
-	GOOGLE_MAPS_API_KEY: process.env['GOOGLE_MAPS_API_KEY']
+	GOOGLE_MAPS_API_KEY: ''
 }
