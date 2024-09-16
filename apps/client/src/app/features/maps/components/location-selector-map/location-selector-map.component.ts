cea2b68ab79917d0ab81a465e0d326b247963b9b
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-09-16 18:25:37 -0400

    fix: store google maps api key in javascript file

--- apps/client/src/app/features/maps/components/location-selector-map/location-selector-map.component.ts
+++ apps/client/src/app/features/maps/components/location-selector-map/location-selector-map.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core'
 import { GoogleMap, MapMarker } from '@angular/google-maps'
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser'
 import { UcabLocationService } from '~/features/maps/ucab-location.service'
-import { environment } from '~env/environment.development'
 
 @Component({
 	selector: 'location-selector-map',
@@ -12,18 +10,7 @@ import { environment } from '~env/environment.development'
 	styleUrl: './location-selector-map.component.css'
 })
 export class LocationSelectorMapComponent {
-	mapSrc: SafeResourceUrl
-
-	constructor(
-		readonly ucabLocationService: UcabLocationService,
-		private readonly sanitizer: DomSanitizer
-	) {
-		this.ucabLocationService.getUcabLocationString
-		const GOOGLE_MAPS_API_KEY = environment.GOOGLE_MAPS_API_KEY
-		const unsafeMapSrc = `//www.google.com/maps/embed/v1/place?language=es&key=${GOOGLE_MAPS_API_KEY}&q=${this.ucabLocationService.getUcabLocationString()}`
-
-		this.mapSrc = this.sanitizer.bypassSecurityTrustResourceUrl(unsafeMapSrc)
-	}
+	constructor(readonly ucabLocationService: UcabLocationService) {}
 
 	center: google.maps.LatLngLiteral = {
 		lat: 8.2973336,
--- apps/client/src/environments/environment.ts
+++ apps/client/src/environments/environment.ts
@@ -1,7 +1,4 @@
-import { GOOGLE_MAPS_API_KEY } from '~root/secrets'
-
 export const environment = {
 	IS_PRODUCTION: true,
-	BACKEND_BASE_URL: 'https://backend.movic.sustentabilidadtech.lat',
-	GOOGLE_MAPS_API_KEY: GOOGLE_MAPS_API_KEY
+	BACKEND_BASE_URL: 'https://backend.movic.sustentabilidadtech.lat'
 }
