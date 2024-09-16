57646d5ab9dffb7c0e7cfdc05f578c08d27d4855
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-09-16 18:00:32 -0400

    fix: store google maps api key in secrets.ts file

--- apps/client/src/environments/environment.development.ts
+++ apps/client/src/environments/environment.development.ts
@@ -1,9 +1,7 @@
-import dotenv from 'dotenv'
-
-dotenv.config()
+import { GOOGLE_MAPS_API_KEY } from '~root/secrets'
 
 export const environment = {
 	IS_PRODUCTION: true,
 	BACKEND_BASE_URL: 'https://backend.movic.sustentabilidadtech.lat',
-	GOOGLE_MAPS_API_KEY: ''
+	GOOGLE_MAPS_API_KEY: GOOGLE_MAPS_API_KEY
 }
