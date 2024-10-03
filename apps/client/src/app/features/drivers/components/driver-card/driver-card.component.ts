85b1c20c631dd826331cc947b44f9ffb27990a86
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-10-03 13:25:10 -0400

    feat: only woman checkbox

---
+++ apps/client/src/app/features/drivers/components/driver-card/driver-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+	selector: 'driver-card',
+	standalone: true,
+	imports: [],
+	templateUrl: './driver-card.component.html',
+	styleUrl: './driver-card.component.css'
+})
+export class DriverCardComponent {}
--- apps/client/src/app/pages/available-drivers/available-drivers.component.ts
+++ apps/client/src/app/pages/available-drivers/available-drivers.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { PageLayoutComponent } from '~/shared/ui/components/page-layout/page-layout.component'
+import { DriverCardComponent } from '../../features/drivers/components/driver-card/driver-card.component'
 
 @Component({
 	selector: 'app-available-drivers',
 	standalone: true,
-	imports: [PageLayoutComponent],
+	imports: [PageLayoutComponent, DriverCardComponent],
 	templateUrl: './available-drivers.component.html',
 	styleUrl: './available-drivers.component.css'
 })
