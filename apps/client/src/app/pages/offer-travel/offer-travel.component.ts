fc171a85146d108273ab83003bcf9568f7a05e69
Chocopaula <mariapforero2003@gmail.com>
2024-10-05 12:01:41 -0400

    feat: add styles in offer-travel

--- apps/client/src/app/pages/offer-travel/offer-travel.component.ts
+++ apps/client/src/app/pages/offer-travel/offer-travel.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core'
+import { GetOwnVehiclesService } from '~/features/vehicles/api/get-own-vehicles.service'
+import { Vehicle } from '~/shared/types/vehicles/vehicle.type'
+import { ButtonComponent } from '~/shared/ui/components/button/button.component'
+import { PageLayoutComponent } from '~/shared/ui/components/page-layout/page-layout.component'
+import { VehicleImageComponent } from '../../features/vehicles/components/vehicle-image/vehicle-image.component'
 
 @Component({
 	selector: 'app-offer-travel',
 	standalone: true,
-	imports: [],
+	imports: [PageLayoutComponent, VehicleImageComponent, ButtonComponent],
 	templateUrl: './offer-travel.component.html',
 	styleUrl: './offer-travel.component.css'
 })
-export class OfferTravelComponent {}
+export class OfferTravelComponent {
+	constructor(private getOwnVehiclesService: GetOwnVehiclesService) {}
+
+	myVehicles: Vehicle[] = []
+
+	ngOnInit() {
+		this.getOwnVehiclesService.execute().subscribe((res) => {
+			this.myVehicles = res.data
+		})
+	}
+}
