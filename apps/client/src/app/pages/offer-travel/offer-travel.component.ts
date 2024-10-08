ae73465dfd40a3a6503c91f5749c436f33749394
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-10-07 21:17:23 -0400

    feat: a

--- apps/client/src/app/pages/offer-travel/offer-travel.component.ts
+++ apps/client/src/app/pages/offer-travel/offer-travel.component.ts
@@ -4,11 +4,17 @@ import { Vehicle } from '~/shared/types/vehicles/vehicle.type'
 import { ButtonComponent } from '~/shared/ui/components/button/button.component'
 import { PageLayoutComponent } from '~/shared/ui/components/page-layout/page-layout.component'
 import { VehicleImageComponent } from '../../features/vehicles/components/vehicle-image/vehicle-image.component'
+import { ModalComponent } from '../../shared/ui/components/modal/modal.component'
 
 @Component({
 	selector: 'app-offer-travel',
 	standalone: true,
-	imports: [PageLayoutComponent, VehicleImageComponent, ButtonComponent],
+	imports: [
+		PageLayoutComponent,
+		VehicleImageComponent,
+		ButtonComponent,
+		ModalComponent
+	],
 	templateUrl: './offer-travel.component.html',
 	styleUrl: './offer-travel.component.css'
 })
@@ -16,10 +22,36 @@ export class OfferTravelComponent {
 	constructor(private getOwnVehiclesService: GetOwnVehiclesService) {}
 
 	myVehicles: Vehicle[] = []
+	selectedVehicle: Vehicle | null = null
+	seatsCount = 0
+	isModalOpen = false
 
 	ngOnInit() {
 		this.getOwnVehiclesService.execute().subscribe((res) => {
 			this.myVehicles = res.data
 		})
+	}
+
+	incrementSeatsCount() {
+		const seatsCountLimit = (this.selectedVehicle?.seatQuantity || 1) - 1
+
+		if (this.seatsCount >= seatsCountLimit) {
+			return
+		}
+
+		this.seatsCount++
+	}
+
+	decrementSeatsCount() {
+		if (this.seatsCount <= 1) {
+			return
+		}
+		this.seatsCount--
+	}
+
+	handleVehicleSelection(vehicle: Vehicle) {
+		this.selectedVehicle = vehicle
+		this.seatsCount = vehicle.seatQuantity - 1
+		this.isModalOpen = true
 	}
 }
