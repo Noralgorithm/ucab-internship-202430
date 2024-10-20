140293a7b2eb960cb92ce03c4528c0e87857bfd2
AlejandroJRosas <alejandrojrosas2001@gmail.com>
2024-10-20 16:43:17 -0400

    feat: show available drivers/travels

--- apps/client/src/app/shared/types/travels/travel.type.ts
+++ apps/client/src/app/shared/types/travels/travel.type.ts
@@ -1,4 +1,5 @@
 import { RideRequest } from '../rides/ride-request.type'
+import { UserProfile } from '../users/user-profile.type'
 import { Vehicle } from '../vehicles/vehicle.type'
 import { Route } from './route.type'
 
@@ -14,6 +15,12 @@ export interface Travel {
 export type TravelType = 'to-ucab' | 'from-ucab'
 
 export type TravelStatus = 'not-started' | 'in-progress' | 'finished'
+
+export type TravelAvailableDriverData = Travel & {
+	vehicle: Vehicle & {
+		driver: UserProfile
+	}
+}
 
 export type TravelLobbyData = Travel & {
 	vehicle: Vehicle
