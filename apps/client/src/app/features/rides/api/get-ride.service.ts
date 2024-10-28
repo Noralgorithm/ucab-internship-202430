55b40a3e108dea381f3a04318796d1a467c4e256
Chocopaula <mariapforero2003@gmail.com>
2024-10-28 00:52:30 -0400

    fix: chore

--- apps/client/src/app/features/rides/api/get-ride.service.ts
+++ apps/client/src/app/features/rides/api/get-ride.service.ts
@@ -3,7 +3,9 @@ import { Injectable } from '@angular/core'
 import { SuccesfulResponse } from '~/shared/types/backend-response.type'
 import { RideTravelData } from '~/shared/types/rides/ride-request.type'
 
+import { map } from 'rxjs'
 import { BYPASS_LOADING } from '~/core/interceptors/loading.interceptor'
+import { constructBackendImageUrl } from '~/shared/utils/construct-backend-image-url.util'
 
 @Injectable({
 	providedIn: 'root'
@@ -14,8 +16,34 @@ export class GetRideService {
 	execute(rideId: string, withLoading = true) {
 		const url = `/rides/${rideId}`
 
-		return this.http.get<SuccesfulResponse<RideTravelData>>(url, {
-			context: new HttpContext().set(BYPASS_LOADING, !withLoading)
-		})
+		return this.http
+			.get<SuccesfulResponse<RideTravelData>>(url, {
+				context: new HttpContext().set(BYPASS_LOADING, !withLoading)
+			})
+			.pipe(map(this.parseResponse))
+	}
+
+	private parseResponse(
+		response: SuccesfulResponse<RideTravelData>
+	): SuccesfulResponse<RideTravelData> {
+		return {
+			status: response.status,
+			data: {
+				...response.data,
+				travel: {
+					...response.data.travel,
+					vehicle: {
+						...response.data.travel.vehicle,
+						driver: {
+							...response.data.travel.vehicle.driver,
+							//TODO: Fix this
+							profilePicSrc: constructBackendImageUrl(
+								response.data.travel.vehicle.driver.profilePicSrc
+							)
+						}
+					}
+				}
+			}
+		}
 	}
 }
