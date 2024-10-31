bd6faee557aa7570943a761e674ff5862ca09615
Chocopaula <mariapforero2003@gmail.com>
2024-10-31 01:19:48 -0400

    feat: i dont know what iam doing

---
+++ apps/client/src/app/features/rides/api/finish-ride.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { SuccesfulResponse } from '~/shared/types/backend-response.type'
+import { GeoJsonPoint } from '~/shared/types/maps/geo-json-points.type'
+
+@Injectable({
+	providedIn: 'root'
+})
+export class FinishRideService {
+	constructor(private readonly http: HttpClient) {}
+
+	execute(finishRideDto: FinishRideServiceDto) {
+		return this.http.post<SuccesfulResponse<FinishRideServiceDto>>(
+			`/rides/${finishRideDto.rideId}/finish-ride`,
+			finishRideDto
+		)
+	}
+}
+
+export interface FinishRideServiceDto {
+	rideId: string
+	dropOff: GeoJsonPoint
+	passengerStarRating: number
+	passengerCommentAfterRide: 'no comments'
+}
