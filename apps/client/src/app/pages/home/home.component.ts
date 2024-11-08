decb599f40d8e50bab8ed26bc6974058cfec2638
Chocopaula <mariapforero2003@gmail.com>
2024-11-08 15:03:28 -0400

    fix: chore

--- apps/client/src/app/pages/home/home.component.ts
+++ apps/client/src/app/pages/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core'
 import { UserCurrentRoleService } from '~/features/profile/user-current-role.service'
 import { ModalComponent } from '~/shared/ui/components/modal/modal.component'
-import { EMERGENCY_NUMBER } from '~root/secrets'
 import { PageLayoutComponent } from '../../shared/ui/components/page-layout/page-layout.component'
 
 @Component({
@@ -18,8 +17,8 @@ export class HomeComponent {
 	//TODO: make component not execute animations on first load when user is already setted as driver
 
 	isUserRoleToggleChecked = false
-	emergencyNumber = EMERGENCY_NUMBER
-	emergencyLink = `sms://${EMERGENCY_NUMBER}?&body=AYUDAAAA ME SECUESTRAN`
+	emergencyNumber = '12345678'
+	emergencyLink = `sms://${this.emergencyNumber}?&body=AYUDAAAA ME SECUESTRAN`
 
 	ngOnInit() {
 		this.userCurrentRoleService.currentRole$.subscribe((role) => {
