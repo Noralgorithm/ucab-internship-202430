6d22e6d3aa4d0c18bd3e62e440ff93c95ab3afba
Chocopaula <mariapforero2003@gmail.com>
2024-11-17 23:20:51 -0400

    fix: chore

--- apps/client/src/app/pages/profile/profile.component.ts
+++ apps/client/src/app/pages/profile/profile.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core'
 import { Router, RouterLink } from '@angular/router'
 import { ToastrService } from 'ngx-toastr'
+import { GetOwnDestinationsService } from '~/features/my-destinations/api/get-own-destinations.service'
 import { GetOwnProfileService } from '~/features/profile/api/get-own-profile.service'
 import { UserCurrentRoleService } from '~/features/profile/user-current-role.service'
 import { TOKEN_KEY } from '~/shared/constants'
+import { Destination } from '~/shared/types/maps/destination'
 import { UserProfile } from '~/shared/types/users/user-profile.type'
 import { ButtonComponent } from '~/shared/ui/components/button/button.component'
 import { PageLayoutComponent } from '~/shared/ui/components/page-layout/page-layout.component'
@@ -22,18 +24,38 @@ export class ProfileComponent {
 		private readonly getOwnProfileService: GetOwnProfileService,
 		public readonly userCurrentRoleService: UserCurrentRoleService,
 		private router: Router,
-		private readonly toastrService: ToastrService
+		private readonly toastrService: ToastrService,
+		private readonly getOwnDestinationsService: GetOwnDestinationsService
 	) {}
 
 	userTypeLabels = USER_TYPE_LABELS
 	userRoleLabels = USER_ROLE_LABELS
 
 	profile: UserProfile | undefined
+	destinations: Destination[] = []
 
 	ngOnInit() {
-		this.getOwnProfileService.execute().subscribe((res) => {
-			this.profile = res.data
+		this.getOwnProfileService.execute().subscribe({
+			next: (res) => {
+				this.profile = res.data
+			},
+			error: (err) => {
+				this.toastrService.error('Error obteniendo el perfil')
+			}
 		})
+
+		this.getOwnDestinationsService.execute().subscribe({
+			next: (res) => {
+				this.destinations = res.data
+			},
+			error: (err) => {
+				this.toastrService.error('Error obteniendo mis destinos')
+			}
+		})
+	}
+
+	redirectToMyDestinations() {
+		this.router.navigate(['/app/my-destinations'])
 	}
 
 	logout() {
