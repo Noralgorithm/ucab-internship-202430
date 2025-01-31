e034a91536d69b6da55dc94c73528b16a569ae6b
Chocopaula <94322466+Paulaforero@users.noreply.github.com>
2025-01-31 02:13:01 -0400

    feat: add update button in available drivers

--- apps/client/src/app/features/drivers/components/driver-card/driver-card.component.ts
+++ apps/client/src/app/features/drivers/components/driver-card/driver-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core'
 import { UserProfile } from '~/shared/types/users/user-profile.type'
+import { getFirstLastName } from '~/shared/utils/get-first-last-name'
+import { getFirstName } from '~/shared/utils/get-first-name'
 
 @Component({
 	selector: 'driver-card',
@@ -12,5 +14,13 @@ export class DriverCardComponent {
 	@Input({ required: true }) driver!: UserProfile & {
 		passengerAmount: number
 		passengerCapacity: number
+	}
+
+	showFirstName(fullName: string) {
+		return getFirstName(fullName)
+	}
+
+	showFirstLastName(fullLastName: string) {
+		return getFirstLastName(fullLastName)
 	}
 }
